b86dd392e950e01458dfaa83b463eb0c6bbc8e8d
Geoff Greer <geoff@greer.fm>
2015-04-05 14:18:31 -0700

    Fix github regex. registered_domains is an object, not an array. Fix package repository url. Remove floorine logging from test.

--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,18 @@
 /*jslint indent: 2, node: true, nomen: true, plusplus: true, todo: true */
 "use strict";
 
-var log = require("floorine");
-
 var fleece = require("../lib/fleece");
 
-let msg_url = "https://github.com/Floobits/fleece";
+let msg_url = "https://github.com/Floobits/floobits-sublime";
 
 fleece.get_url_msg(msg_url, function (err, result) {
   if (err) {
-    log.warn("URL %s error: %s", msg_url, err);
+    console.error("URL %s error: %s", msg_url, err);
     return;
   }
   if (!result) {
-    log.log("URL %s: No result", msg_url);
+    console.log("URL %s: No result", msg_url);
     return;
   }
-  log.log(result);
+  console.log(result);
 });
